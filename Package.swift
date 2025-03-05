597a6b53e377160eae0863de206ae754a6902967
Edon <edon@valdman.works>
2025-03-05 10:01:19 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,13 +9,15 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PlotSVG",
-            targets: ["PlotSVG"]),
+            targets: ["PlotSVG"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PlotSVG"),
+            name: "PlotSVG"
+        ),
         .testTarget(
             name: "PlotSVGTests",
             dependencies: ["PlotSVG"]
