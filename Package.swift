a4993e764a939893fd97a2790d563a87a77b85b1
Edon <edon@valdman.works>
2025-03-05 10:01:36 -0600

    Added `Plot` as SPM dependency

--- Package.swift
+++ Package.swift
@@ -12,11 +12,17 @@ let package = Package(
             targets: ["PlotSVG"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/JohnSundell/Plot.git", from: "0.14.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PlotSVG"
+            name: "PlotSVG",
+            dependencies: [
+                .product(name: "Plot", package: "Plot"),
+            ]
         ),
         .testTarget(
             name: "PlotSVGTests",
