e0f7694270201d5a16b7250fbb2baa897bbc4226
Edon <edon@valdman.works>
2025-03-19 13:02:20 -0500

    Added `SVGLineCap` and `SVGLineJoin` types

---
+++ Sources/PlotSVG/Types/SVGLineCap.swift
@@ -0,0 +1,13 @@
+//
+//  SVGLineCap.swift
+//  PlotSVG
+//
+//  Created by Edon Valdman on 3/19/25.
+//
+
+import Foundation
+
+public enum SVGLineCap: String {
+    case butt, round, square
+//    inherit
+}
---
+++ Sources/PlotSVG/Types/SVGLineJoin.swift
@@ -0,0 +1,13 @@
+//
+//  SVGLineJoin.swift
+//  PlotSVG
+//
+//  Created by Edon Valdman on 3/19/25.
+//
+
+import Foundation
+
+public enum SVGLineJoin: String {
+    case miter, round, bevel
+    //    inherit
+}
