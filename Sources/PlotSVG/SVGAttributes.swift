c4eab4e2901c128516193dd4fb7cf596124837a1
Edon <edon@valdman.works>
2025-03-19 13:21:42 -0500

    Update SVGAttributes.swift
    Added `Node` versions of SVG element attributes.

--- Sources/PlotSVG/SVGAttributes.swift
+++ Sources/PlotSVG/SVGAttributes.swift
@@ -66,6 +66,64 @@ public extension Attribute where Context == SVG.DocumentContext {
     }
 }
 
+public extension Node where Context == SVG.DocumentContext {
+    static func width(_ width: SVGLength) -> Node {
+        .attribute(
+            named: "width",
+            value: width.rawValue
+        )
+    }
+    
+    static func height(_ height: SVGLength) -> Node {
+        .attribute(
+            named: "height",
+            value: height.rawValue
+        )
+    }
+    
+    static func x(_ x: SVGLength) -> Node {
+        .attribute(
+            named: "x",
+            value: x.rawValue
+        )
+    }
+    
+    static func y(_ y: SVGLength) -> Node {
+        .attribute(
+            named: "y",
+            value: y.rawValue
+        )
+    }
+    
+    static func preserveAspectRatio(
+        _ option: PreserveAspectRatio.Options,
+        meetOrSlice: PreserveAspectRatio.MeetSlice?
+    ) -> Node {
+        return .attribute(
+            named: "preserveAspectRatio",
+            value: PreserveAspectRatio(
+                option: option,
+                meetOrSlice: meetOrSlice
+            ).rawValue
+        )
+    }
+    
+    static func viewBox<F: BinaryFloatingPoint>(
+        minX: F = 0,
+        minY: F = 0,
+        width: F,
+        height: F
+    ) -> Node {
+        .attribute(
+            named: "viewBox",
+            value: [minX, minY, width, height]
+                .map { Double($0) }
+                .map { String($0) }
+                .joined(separator: " ")
+        )
+    }
+}
+
 public extension Node where Context: SVGClassableContext {
     static func `class`(_ className: String...) -> Node {
         .attribute(named: "class", value: className.joined(separator: " "))
