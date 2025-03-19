5b93fa10c171b675bf3b45be4562e6959d840096
Edon <edon@valdman.works>
2025-03-19 12:49:44 -0500

    Update SVGLength.swift
    Refactored to use `CGFloat`.

--- Sources/PlotSVG/Types/SVGLength.swift
+++ Sources/PlotSVG/Types/SVGLength.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 public struct SVGLength: Hashable {
-    public let length: Double
+    public let length: CGFloat
     public let unit: UnitType
     
-    init(_ length: Double, unit: UnitType) {
+    private init(_ length: CGFloat, unit: UnitType) {
         self.length = length
         self.unit = unit
     }
@@ -44,49 +44,49 @@ public struct SVGLength: Hashable {
     }
     
     /// No unit type was provided (i.e., a unitless value was specified), which indicates a value in user units.
-    public static func number(_ length: Double) -> SVGLength {
+    public static func number(_ length: CGFloat) -> SVGLength {
         return .init(length, unit: .number)
     }
     /// A percentage value was specified.
-    public static func percentage(_ length: Double) -> SVGLength {
+    public static func percentage(_ length: CGFloat) -> SVGLength {
         return .init(length, unit: .percentage)
     }
         /// A value was specified using the em units defined in CSS2.
-    public static func em(_ length: Double) -> SVGLength {
+    public static func em(_ length: CGFloat) -> SVGLength {
         return .init(length, unit: .em)
     }
         /// A value was specified using the ex units defined in CSS2.
-    public static func ex(_ length: Double) -> SVGLength {
+    public static func ex(_ length: CGFloat) -> SVGLength {
         return .init(length, unit: .ex)
     }
         /// A value was specified using the px units defined in CSS2.
-    public static func px(_ length: Double) -> SVGLength {
+    public static func px(_ length: CGFloat) -> SVGLength {
         return .init(length, unit: .px)
     }
         /// A value was specified using the cm units defined in CSS2.
-    public static func cm(_ length: Double) -> SVGLength {
+    public static func cm(_ length: CGFloat) -> SVGLength {
         return .init(length, unit: .cm)
     }
         /// A value was specified using the mm units defined in CSS2.
-    public static func mm(_ length: Double) -> SVGLength {
+    public static func mm(_ length: CGFloat) -> SVGLength {
         return .init(length, unit: .mm)
     }
         /// A value was specified using the in units defined in CSS2.
-    public static func `in`(_ length: Double) -> SVGLength {
+    public static func `in`(_ length: CGFloat) -> SVGLength {
         return .init(length, unit: .in)
     }
         /// A value was specified using the pt units defined in CSS2.
-    public static func pt(_ length: Double) -> SVGLength {
+    public static func pt(_ length: CGFloat) -> SVGLength {
         return .init(length, unit: .pt)
     }
         /// A value was specified using the pc units defined in CSS2.
-    public static func pc(_ length: Double) -> SVGLength {
+    public static func pc(_ length: CGFloat) -> SVGLength {
         return .init(length, unit: .pc)
     }
 }
 
 extension SVGLength: ExpressibleByFloatLiteral {
     public init(floatLiteral value: Double) {
-        self.init(value, unit: .number)
+        self.init(CGFloat(value), unit: .number)
     }
 }
